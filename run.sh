23b5d969c1a94bb04a7b5d0db5bed3aad3185011
Lean-Y <yueling.me@qq.com>
2023-05-23 13:52:38 +0800

    add run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 python train.py -d ddi -e ddi_hop3 --gpu=0 --hop=3 --batch=256 --emb_dim=32 -b=10
+
+# python train.py -d drugbank -e drugbank-default2 --hop=2 --batch=256 --emb_dim=32 -b=10 --gpu 6
